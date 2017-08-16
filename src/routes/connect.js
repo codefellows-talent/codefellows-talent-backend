f833afff55fdadff6353398a26103a2bd8602052
Ryan Felix <therjfelix@gmail.com>
2017-08-16 16:32:30 -0700

    rate limit emails

--- src/routes/connect.js
+++ src/routes/connect.js
@@ -2,11 +2,34 @@ import { Router } from 'express';
 import { json } from 'body-parser';
 
 import appCache from '../model/cache.js';
-import { sendConnectionEmail, sendClientEmail } from '../lib/email.js';
+import { sendConnectionEmail as _sendConnectionEmail, sendClientEmail as _sendClientEmail } from '../lib/email.js';
 
 const uri = `${process.env.API_URI}/connect`;
 
 const connectRouter = new Router();
+
+const rateLimit = (fn, frequency) => {
+  let queue = [];
+  let interval = null;
+  return (...args) => {
+    const callFn = () => fn(...args);
+    if(!interval) {
+      interval = setInterval(() => {
+        if(queue.length > 0) {
+          queue.shift()();
+        } else {
+          clearInterval(interval);
+          interval = null;
+        }
+      }, frequency);
+      callFn();
+    }
+    queue.push(callFn);
+  };
+};
+
+const sendConnectionEmail = rateLimit(_sendConnectionEmail, 5000);
+const sendClientEmail = rateLimit(_sendClientEmail, 5000);
 
 connectRouter.post(uri, json(), (req, res) => {
   if(!(req.body.email && req.body.company && req.body.name)) {
