b21ed4492d80fcecdddc09bea6a7c0e56bea4ba7
Ryan Felix <therjfelix@gmail.com>
2017-08-16 11:34:29 -0700

    require and use client email on connect route

--- src/lib/email.js
+++ src/lib/email.js
@@ -10,7 +10,7 @@ const formatProfileForEmail = profile => (
   `${profile.nickname}, ${profile.email} (Salesforce ID# ${profile.salesforceId})`
 );
 
-const sendConnectionEmail = profiles => {
+export const sendConnectionEmail = profiles => {
   const formattedProfiles = profiles.map(formatProfileForEmail);
   const bodyText = formattedProfiles.reduce((message, text) => message.concat(text).concat('\n'), '');
   return new Promise((resolve, reject) =>
@@ -37,4 +37,27 @@ const sendConnectionEmail = profiles => {
   );
 };
 
-export default sendConnectionEmail;
+export const sendClientEmail = email => {
+  const bodyText = 'Your request to connect with Code Fellows graduates was approved. Thank you!';
+  return new Promise((resolve, reject) =>
+    ses.sendEmail({
+      Source: fromAddress,
+      Destination: { ToAddresses: [email] },
+      Message: {
+        Subject: {
+          Data: 'Your request to connect with Code Fellows graduates was received',
+        },
+        Body: {
+          Text: {
+            Data: bodyText,
+          },
+        },
+      },
+    }, function(err, data) {
+      if(err)
+        reject(err);
+      console.log(`Email sent to hiring partner at ${email}`);
+      resolve(data);
+    })
+  );
+};
--- src/routes/connect.js
+++ src/routes/connect.js
@@ -2,16 +2,20 @@ import { Router } from 'express';
 import { json } from 'body-parser';
 
 import appCache from '../model/cache.js';
-import sendEmail from '../lib/email.js';
+import { sendConnectionEmail, sendClientEmail } from '../lib/email.js';
 
 const uri = `${process.env.API_URI}/connect`;
 
 const connectRouter = new Router();
 
 connectRouter.post(uri, json(), (req, res) => {
-  console.log('hit post route');
+  if(!req.body.email) {
+    res.status(400)
+      .send('No client email field was included. A client email is required to connect with graduates.');
+  }
   Promise.all(req.body.ids.map(id => appCache.get(id)))
-    .then(profiles => sendEmail(profiles))
+    .then(profiles => sendConnectionEmail(profiles))
+    .then(() => sendClientEmail(req.body.email))
     .then(() => res.sendStatus(200))
     .catch((err) => {
       console.error(err);
