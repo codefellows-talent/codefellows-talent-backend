04a9e4e5c48044d88ecdc57c5d322b3db1d2bcdb
Ryan Felix <therjfelix@gmail.com>
2017-08-17 14:23:11 -0700

    setup jest correctly, add a simple server test

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,6 +1,15 @@
-process.env.PORT = 7777;
+process.env.PORT = 3002;
 process.env.MONGODB_URI = (
   process.env.TRAVIS
     ? process.env.MONGODB_URI
     : 'mongodb://localhost/test-db'
 );
+process.env.API_URI = '/api/v1';
+process.env.AWS_ACCESS_KEY_ID='mock access key';
+process.env.AWS_SECRET_ACCESS_KEY='mock secret key';
+process.env.S3_CSV_URI='https://s3.amazonaws.com/401-hire-cf/sample.csv';
+process.env.EMAIL_SOURCE='cfhired@zoho.com';
+process.env.EMAIL_TARGET='therjfelix@gmail.com';
+process.env.AWS_REGION='us-west-2';
+process.env.EMAIL_RATE_LIMIT=10000;
+process.env.CACHE_UPDATE_INTERVAL=120000;
